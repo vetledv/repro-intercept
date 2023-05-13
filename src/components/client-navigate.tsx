64e4cbf84a1ad926e7c910f34e8a64b7715a8a9c
Vetle de Vries <vetle1608@gmail.com>
2023-05-13 02:11:55 +0200

    remove unused components

--- src/components/client-navigate.tsx
+++ src/components/client-navigate.tsx
@@ -1,24 +1,6 @@
 "use client";
 import { useRouter } from "next/navigation";
 
-export const ClientBackButton = () => {
-	const router = useRouter();
-	return (
-		<button className="w-fit" onClick={() => router.back()}>
-			router.back 🟢
-		</button>
-	);
-};
-
-export const ClientHelloButtonPush = () => {
-	const router = useRouter();
-	return (
-		<button className="w-fit" onClick={() => router.push("/hello")}>
-			router.push("/hello") 🟢
-		</button>
-	);
-};
-
 export const ClientRouterNavButton = ({
 	href,
 	emoji,
